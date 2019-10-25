28f62bc8cf4201fb440e11d5cb334e2a986e7e63
Arya Wiratama <aryawiratama99@gmail.com>
2019-10-25 14:40:09 +0800

    response

--- modules/linebot/src/Controller/FE/LineBotController.php
+++ modules/linebot/src/Controller/FE/LineBotController.php
@@ -25,14 +25,43 @@ use LINE\LINEBot\Exception\InvalidEventRequestException;
 use LINE\LINEBot\Exception\InvalidSignatureException;
 
 class LineBotController extends ControllerBase {
-    public function home() {
+    protected $state;
+
+    public function __construct()
+    {
+        $this->state = [
+            '1' => 'first_follow',
+            '2' => 'send_otp',
+            '3' => 'validate_otp',
+            '4' => 'offer_subscription',
+            '5' => 'paid_subscription',
+            '6' => 'list_voucher',
+            '7' => 'expired_subscription_reminder',
+            '8' => 'expired_subscription',
+            '9' => 'extend_subscription',
+            '10' => 'paid_extend_subscription',
+            '11' => 'show_card_list',
+            '12' => 'show_card_detail',
+            '13' => 'show_voucher_list',
+            '14' => 'show_voucher_used',
+            '15' => 'show_expired_subscribtion',
+            '16' => 'show_active_subscribtion',
+            '17' => 'show_subscribtion',
+            '18' => 'redirect_to_tada',
+            '19' => 'back_from_tada',
+        ];
+    }
+
+    public function home()
+    {
         echo "index";
         // var_dump(file_exists(getcwd().'/modules/linebot/config/creds'));
         
         die();
     }
     
-    public function responseMessage() {
+    public function responseMessage()
+    {
         $basic_id = \Drupal::request()->get('bot_id');
         $basic_id = Html::escape(Xss::filter($basic_id));
 
@@ -58,6 +87,7 @@ class LineBotController extends ControllerBase {
         }
         
         foreach ($events as $key_event=>$event) {
+            $current_state = $this->getCurrentState();
             $lineId = $event->getUserId();
             
             if (!($event instanceof MessageEvent)) {
@@ -70,23 +100,31 @@ class LineBotController extends ControllerBase {
             
             $replyText = $event->getText();
             if ($event instanceof FollowEvent) {
-                $state = 'greeting';
+                $state = 1;
             } else {
                 $is_regirtered = $this->getuserLineID($lineId);
                 if(!$is_regirtered) {
-                    $state = 'greeting';
+                    $state = 1;
                 }
             }
+
+            if($current_state==1 and $this->validPhone($input_message)) {
+                $state = 2;
+            }
+
+            if($current_state==2) {
+                $state = 3;
+            }
             
-            $dialogFlowResponse = $this->botState($state, $replyText);
-            // $bot->replyText($event->getReplyToken(), $dialogFlowResponse['responseText']);
-            $bot->replyText($event->getReplyToken(), 'dialog_flow_data :'.json_encode($dialogFlowResponse));
+            $currentState = $this->botResponse($state, $replyText);
+            $bot->replyText($event->getReplyToken(), $currentState);
         }
 
         return new JsonResponse('Ok', 200);
     }
 
-    private function getMerchantData($basic_id = null) {
+    private function getMerchantData($basic_id = null)
+    {
         $merchant = [];
 
         $merchant_data = \Drupal::service('entity.query')->get('node');
@@ -120,18 +158,77 @@ class LineBotController extends ControllerBase {
         return $results;
     }
 
-    private function botState($state = null, $input_message = '')
+    private function botResponse($state = null, $input_message = '')
     {
-        $currentState = [];
-        
-        if($state == 'greeting') {
-            $linebot_service_dialogflow = \Drupal::service('linebot_service.dialogflow');
-            $params = array(
-                'text' => $input_message,
-            );
-            $currentState = $linebot_service_dialogflow->getDialogFlowIntents($params);
+        $dialogFlowResponse = [];
+        $linebot_service_dialogflow = \Drupal::service('linebot_service.dialogflow');
+        $params = array(
+            'text' => $input_message,
+        );
+        $dialogFlowResponse = $linebot_service_dialogflow->getDialogFlowIntents($params);
+
+        switch ($dialogFlowResponse['intentName']) {
+            case 'greetingIntent':
+                $responseDialogFlow = $dialogFlowResponse['responseText'];
+                break;
+            
+            case 'fallBackIntent':
+                $responseDialogFlow = $dialogFlowResponse['responseText'];
+                break;
+            
+            default:
+                $responseDialogFlow = '';
+                break;
         }
 
-        return $currentState;
+        switch ($state) {
+            case '1':
+                $stateResponse = $responseDialogFlow.'
+Silakan masukkan nomor handphone Anda untuk melanjutkan. Contoh: +6281312345938';
+                $this->setCurrentState($state);
+                break;
+            
+            case '2':
+                $stateResponse = $stateResponse = 'Masukkan kode verifikasi yang sudah kami kirimkan melalui SMS ke nomer '.$input_message;
+                $this->setCurrentState($state);
+                break;
+            
+                case '2':
+                $stateResponse = $stateResponse = 'Terimakasih. Kami sedang menvalidasi kode otp anda, mohon menunggu';
+                $this->setCurrentState($state);
+                break;
+
+            default:
+                $stateResponse = '';
+                break;
+        }
+
+        return $response;
     }
+
+    private function setCurrentState($state = null)
+    {
+        $session = \Drupal::request()->getSession();
+        $session->set('current_bot_state', $state);
+
+        return true;
+    }
+
+    private function getCurrentState()
+    {
+        $session = \Drupal::request()->getSession();
+        $state = $session->get('current_bot_state');
+
+        return ($state) ? $state : null;
+    }
+
+    private function validPhone($phone = ''){
+        $patern = "/(\()?(\+62|62|0)(\d{2,3})?\)?[ .-]?\d{2,4}[ .-]?\d{2,4}[ .-]?\d{2,4}/";    
+        $is_phone = false;
+        if (preg_match($patern, $phone)) {
+            $is_phone = true;
+        }
+    
+        return $is_phone;
+      }    
 }
